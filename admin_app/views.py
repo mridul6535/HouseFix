ca3fbe4368af89644c9b9e4e1074dfdb21ee9db8
mridul6535 <mridhulkp7@gmail.com>
2026-03-08 12:47:02 +0530

    redirect 2

--- admin_app/views.py
+++ admin_app/views.py
@@ -25,7 +25,8 @@ def admin_login(request):
         name = request.POST.get("admin_name")
         mail = request.POST.get("admin_email")
         pswd = request.POST.get("password")
-        redirect(dashboard)
+        redirect('')
+        redirect(worker_login_page)
 
         user = authenticate(request, username=name, password=pswd)
 
