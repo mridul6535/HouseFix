a47f18999cf6913e572b6077a61d469d541214db
mridul6535 <mridhulkp7@gmail.com>
2026-03-18 11:57:24 +0530

    debug and secret key

--- HouseFix/settings.py
+++ HouseFix/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l@d!&7eoc-*luk&@4*%zh1@iy+xffx88f1r6%7r*(^)#84utk='
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('DEBUG') == 'True'
 
 ALLOWED_HOSTS = ["*"]
 
