882da5a64a3f5c2a9565221fcab155fa1d4ed7c7
mridul6535 <mridhulkp7@gmail.com>
2026-03-24 12:07:31 +0530

    migrate

--- web_app/models.py
+++ web_app/models.py
@@ -13,7 +13,7 @@ class WorkerDB(models.Model):
     user = models.OneToOneField(Users, on_delete=models.CASCADE)
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=255)
-    email = models.CharField(max_length=20)
+    email = models.EmailField()
     mobile = models.CharField(max_length=20)
     category = models.CharField(max_length=50)
     city = models.CharField(max_length=100)
